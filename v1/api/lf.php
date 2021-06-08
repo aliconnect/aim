bf744cd2a7f7298042230d406daec519ccd882ef
Max van Kampen <max.van.kampen@alicon.nl>
2021-06-08 15:40:12 +0200

    Auto commit

--- v1/api/lf.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo file_get_contents($_SERVER['DOCUMENT_ROOT'].'/'.$_GET[fname]);
-?>
--- v1/api/rc.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//err($aim);
-	$html = file_get_contents($_SERVER[DOCUMENT_ROOT]."/sites/rc/app/v1/index.htm");
-	
-	die(str_replace("</head","<script>client_id='$aim->filename';</script></head",$html));
-?>
